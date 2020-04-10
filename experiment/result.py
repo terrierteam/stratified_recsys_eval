bd4fe2601dbdf0d36cb6750ecf9b6cbf72d8b446
Amir H. Jadidinejad <amir.jadidi@yahoo.com>
2020-04-11 00:53:27 +0100

    bug fixed: _table_format function modifies the input data

--- experiment/result.py
+++ experiment/result.py
@@ -43,11 +43,11 @@ class STResult(list):
         data = [[NUM_FMT.format(v) for v in row] for row in data]
         index.extend(['Unbiased'])
 
-        self.table = _table_format(
-            data, headers, index, h_bars=[1, 2, len(data)])
-
         # add unbiased to the list
         self.append(Result(model_name=self[0].model_name,
                            metric_avg_results=OrderedDict(
                                zip(headers, unbiased)),
                            metric_user_results=None))
+
+        self.table = _table_format(
+            data, headers, index, h_bars=[1, 2, len(data)])
